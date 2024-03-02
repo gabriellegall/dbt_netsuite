c22b43fff840ed7b0415d82e5dc4dc093a0ec68f
Gabriel LE GALL <gabrielgall@hotmail.fr>
2024-03-02 12:07:43 +0100

    incremental load

---
+++ models/dwh/transaction_with_line.sql
@@ -0,0 +1,23 @@
+
+{{
+    config(
+        materialized='incremental',
+        unique_key = ['transaction_nsid', 'transaction_line_nsid']
+    )
+}}
+
+SELECT 
+    t.transaction_nsid
+    , tl.transaction_line_nsid
+    , t.transaction_type
+    , t.transaction_number
+    , t.transaction_date
+    , t.transaction_last_modified_date
+
+FROM {{ ref('transaction') }} t
+    LEFT OUTER JOIN {{ ref('transactionline') }} tl
+    ON t.transaction_nsid = tl.transaction_nsid
+
+{% if is_incremental() %}
+    WHERE t.transaction_last_modified_date >= (SELECT MAX(incremental_date.transaction_last_modified_date) FROM {{ this }} as incremental_date) 
+{% endif %}
