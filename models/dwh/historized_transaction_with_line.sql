61bfb9be890f21fbf3eeb578f7cfd66ae254464d
Gabriel LE GALL <gabrielgall@hotmail.fr>
2024-03-10 13:11:27 +0100

    devs

--- models/dwh/historized_transaction_with_line.sql
+++ models/dwh/historized_transaction_with_line.sql
@@ -1,20 +1,22 @@
 {{
     config (
-        materialized        = 'incremental'    
+        full_refresh        = false
+        , materialized      = 'incremental'    
         , on_schema_change  = 'append_new_columns'
-        , full_refresh      = false
     )
 }}
 
 SELECT
+    {# Previous incremental logging information is not relevant to keep for the snapshotting - i.e. load times, run id #}
     {{ dbt_utils.star(from=ref('transaction_with_line'), except=[var("dbt_run_id_col_name"), var("dbt_load_datetime_col_name")]) }}
-    , {{ column_dbt_previous_month() }} AS {{ var("dbt_prev_month_col_name") }}
-    , {{ column_dbt_load_datetime() }}  AS {{ var("dbt_load_datetime_col_name") }}
-    , '{{ var("dbt_run_id") }}'         AS {{ var("dbt_run_id_col_name") }}
+    
+    , {{ column_dbt_previous_month() }} AS {{ var("dbt_snapshot_col_name") }}     
+    , {{ column_dbt_load_datetime() }}  AS {{ var("dbt_load_datetime_col_name") }} {# Load date is set to the current dbt time for all records inserted #}
+    , '{{ var("dbt_run_id") }}'         AS {{ var("dbt_run_id_col_name") }}         
 
 FROM {{ ref('transaction_with_line') }} t
 
 {% if is_incremental() %}
-        {# Checks if a snapshot has been made #}
-    WHERE {{ column_dbt_previous_month() }} >= ( SELECT MAX ( incremental_date.{{ var("dbt_prev_month_col_name") }} ) FROM {{ this }} as incremental_date )
+    {# Checks if a snapshot has been made #}
+    WHERE {{ column_dbt_previous_month() }} > ( SELECT MAX ( incremental_date.{{ var("dbt_snapshot_col_name") }} ) FROM {{ this }} as incremental_date )
 {% endif %}
--- models/prp/prep_transaction_with_lines_for_union.sql
+++ models/prp/prep_transaction_with_lines_for_union.sql
@@ -7,6 +7,6 @@
 
 SELECT 
     *
-    , {{ column_dbt_load_datetime() }}  AS {{ var("dbt_prev_month_col_name") }} {# for current data, snapshot date is set to now #}
+    , {{ column_dbt_load_datetime() }} AS {{ var("dbt_snapshot_col_name") }} {# for current data, snapshot date is set to now #}
     
 FROM {{ ref('transaction_with_line') }} t
