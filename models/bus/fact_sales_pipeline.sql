71ab73f5cb75f3276656c77c5390a338be12f6c4
Gabriel LE GALL <gabrielgall@hotmail.fr>
2024-03-11 21:21:47 +0100

    customer integration

--- models/bus/fact_sales_pipeline.sql
+++ models/bus/fact_sales_pipeline.sql
@@ -7,8 +7,9 @@
 SELECT 
     {{ dbt_utils.star(from=ref('transaction_with_line')) }}
     , CASE WHEN t.transaction_type IN ('Opportunity', 'Sales Order') THEN t.expected_delivery_date ELSE t.transaction_date END AS calculation_date
-    , {{ dbt_utils.star(from=ref('dim_item'), except = var("scd_excluded_col_name") ) }}
-    , {{ dbt_utils.star(from=ref('dim_bu'), except = var("scd_excluded_col_name") ) }}
+    , {{ dbt_utils.star(from=ref('dim_item')    , except = var("scd_excluded_col_name") ) }}
+    , {{ dbt_utils.star(from=ref('dim_bu')      , except = var("scd_excluded_col_name") ) }}
+    , {{ dbt_utils.star(from=ref('dim_customer'), except = var("scd_excluded_col_name") ) }}
 
 FROM {{ ref("fact_all_transactions_with_line") }} t
 
