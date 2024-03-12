a8b703697efa3e8613123bbdf8edfadde4761fcf
Gabriel LE GALL <gabrielgall@hotmail.fr>
2024-03-12 11:17:53 +0100

    de-centralization

--- models/bus/fact_all_transactions_with_line.sql
+++ models/bus/fact_all_transactions_with_line.sql
@@ -20,25 +20,16 @@ data_consolidation AS
     SELECT 
         t.* 
         , DENSE_RANK () OVER ( ORDER BY {{ var("dbt_snapshot_col_name") }} DESC )                    AS snapshot_date_order
-        , {{ dbt_utils.star(from=ref('dim_item'), except = var("scd_excluded_col_name") ) }}
         , {{ dbt_utils.star(from=ref('dim_bu'), except = var("scd_excluded_col_name") ) }}
-        , {{ dbt_utils.star(from=ref('dim_customer'), except = var("scd_excluded_col_name") ) }}
         , COALESCE( fx_dated.fx_rate_original_to_usd, fx_latest.fx_rate_original_to_usd )            AS fx_rate_original_to_usd
         , COALESCE( fx_dated.fx_rate_original_to_dynamic, fx_latest.fx_rate_original_to_dynamic )    AS fx_rate_original_to_dynamic
         , '{{ var("fx_avg_implicit_currency") }}'                                                    AS dynamic_target_currency
 
     FROM union_current_and_snapshot t
-    
-    {# Dimensions #}
-    LEFT OUTER JOIN {{ ref("dim_item") }} it
-        ON it.pk_{{ var("item_key") }} = t.fk_{{ var("item_key") }}
-        AND t.transaction_date BETWEEN it.scd_valid_from_fill_date AND it.scd_valid_to_fill_date
+    {# Common dimensions for all tansaction types #}
     LEFT OUTER JOIN {{ ref("dim_bu") }} bu
         ON bu.pk_{{ var("business_unit_key") }} = t.fk_{{ var("business_unit_key") }}
         AND t.transaction_date BETWEEN bu.scd_valid_from_fill_date AND bu.scd_valid_to_fill_date
-    LEFT OUTER JOIN {{ ref("dim_customer") }} cu
-        ON cu.pk_{{ var("customer_key") }} = t.fk_{{ var("customer_key") }}
-        AND t.transaction_date BETWEEN cu.scd_valid_from_fill_date AND cu.scd_valid_to_fill_date
     {# Fx rates #}
     LEFT OUTER JOIN {{ ref("fact_fx_avg_rate_latest") }} fx_latest
         ON bu.hist_bu_currency = fx_latest.original_currency
--- models/bus/fact_sales_pipeline.sql
+++ models/bus/fact_sales_pipeline.sql
@@ -4,17 +4,19 @@
     )
 }}
 
-
-{# TO DO : Make the joins inside this layer instead of the previous one, this way you can retrieve all columns dynamically from the previous layer #}
-
 SELECT 
-    {{ dbt_utils.star(from=ref('transaction_with_line')) }}
+    {{ dbt_utils.star(from=ref('fact_all_transactions_with_line')) }}
     , CASE WHEN t.transaction_type IN ('Opportunity', 'Sales Order') THEN t.expected_delivery_date ELSE t.transaction_date END AS calculation_date
     , {{ dbt_utils.star(from=ref('dim_item')    , except = var("scd_excluded_col_name") ) }}
-    , {{ dbt_utils.star(from=ref('dim_bu')      , except = var("scd_excluded_col_name") ) }}
     , {{ dbt_utils.star(from=ref('dim_customer'), except = var("scd_excluded_col_name") ) }}
 
 FROM {{ ref("fact_all_transactions_with_line") }} t
+LEFT OUTER JOIN {{ ref("dim_customer") }} cu
+    ON cu.pk_{{ var("customer_key") }} = t.fk_{{ var("customer_key") }}
+    AND t.transaction_date BETWEEN cu.scd_valid_from_fill_date AND cu.scd_valid_to_fill_date
+LEFT OUTER JOIN {{ ref("dim_item") }} it
+    ON it.pk_{{ var("item_key") }} = t.fk_{{ var("item_key") }}
+    AND t.transaction_date BETWEEN it.scd_valid_from_fill_date AND it.scd_valid_to_fill_date
 
 {# Scope of the dataset #}
 WHERE t.transaction_type IN ( {{ var("sales_scope_type") | join(', ') }} )
