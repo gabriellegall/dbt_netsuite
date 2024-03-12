c2ff931c3dfb0371f745b62acb0046b82c2085f3
Gabriel LE GALL <gabrielgall@hotmail.fr>
2024-03-12 21:12:53 +0100

    dataset creation

--- models/ds/dataset_sales_pipeline_monitoring.sql
+++ models/ds/dataset_sales_pipeline_monitoring.sql
@@ -6,7 +6,7 @@
 
 {{ dbt_utils.union_relations (
     relations = [
-        ref('fact_sales_pipeline'),
+        ref('dataset_sales_pipeline'),
         ref('prep_budget_for_union')
         ],
     exclude=["_dbt_source_relation"]
